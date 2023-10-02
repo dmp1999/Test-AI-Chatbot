928b1bed2ba5c642eba28d6e3676000ba70d138b
dmp1999 <dhruvilparikh79@gmail.com>
2023-10-02 14:41:15 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, session, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    app.run()
